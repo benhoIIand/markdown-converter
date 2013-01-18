0b47c340b2207e53f5f31a9162a7bd6b12ee316b
hollandben <bholland@opentable.com>
2013-01-18 00:12:22 +0000

    Moved preview into iframe. Cached JS objects and reformated some JS code. Bossed it basically

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 	<title>Markdown Converter</title>
 
 	<link rel="stylesheet" href="css/page.css">
-	<link rel="stylesheet" href="css/markdown.css">
 </head>
 <body>
 
@@ -14,22 +13,21 @@
 		<span id="add_css">Add Custom CSS</span>
 	</div>
 
-	<textarea id="code_view" placeholder="Enter markdown content"></textarea>
-	<div id="preview" class="markdown-body"></div>
+	<textarea id="code_view" class="view" placeholder="Enter markdown content"></textarea>
+	<div id="ruler" class="ruler"></div>
+	<iframe src="iframe.html" id="preview" class="view"></iframe>
 
 	<div id="dialog_overlay"></div>
-	<div id="css_dialog" class="dialog">
+	<div id="dialog_css" class="dialog">
 		<textarea id="css_textarea" placeholder="Enter your CSS"></textarea>
 		<div class="dialog-menu">
-			<label for="remember_css">Remember CSS</label><input type="checkbox" id="remember_css">
 			<div class="buttons">
 				<button id="btn_clear" class="btn btn-clear">Clear</button>
 				<button id="btn_save" class="btn">Save</button>
 			</div>
+			<label for="remember_css">Remember CSS</label><input type="checkbox" id="remember_css">
 		</div>
 	</div>
-
-	<style id="extra_styles"></style>
 
 	<script src="js/jquery.min.js"></script>
 	<script src="js/converter.js"></script>
--- js/converter.js
+++ js/converter.js
@@ -1,46 +1,123 @@
-var delay;
+var delay, windowWidth;
 
-function setDelay() {
+var $textarea, $addCSS, $btnSave, $btnClear, $dialogOverlay, $dialogyCSS, $codeView, $rememberCSSCheckbox, $extraStylesContainer, $cssTextarea, $ruler, $preview, $window;
+
+var setDelay = function() {
     clearTimeout(delay);
     delay = setTimeout(function() {
         getPreview();
     }, 400);
 }
 
-function getPreview() {
+var getPreview = function() {
     $.ajax({
         type: 'post',
         url: 'converter.php',
-        data: 'content='+encodeURIComponent($('#code_view').val()),
+        data: 'content='+encodeURIComponent($codeView.val()),
         success: function(r) {
-            $('#preview').html(r);
+            $preview.html(r);
         }
     });
 }
 
+var extendJquery = function() {
+    $.fn.extend({
+        disableSelection: function() {
+            return this.bind( ( $.support.selectstart ? "selectstart" : "mousedown" ) +
+                ".ui-disableSelection", function( event ) {
+                    event.preventDefault();
+                });
+        },
+
+        enableSelection: function() {
+            return this.unbind( ".ui-disableSelection" );
+        }
+    });
+}
+
+var showHideDialog = function() {
+    $dialogyCSS.fadeToggle();
+    $dialogOverlay.fadeToggle();
+}
+
+var saveStyles = function() {
+    if($rememberCSSCheckbox.is(':checked')) {
+        localStorage.setItem('css-styles', JSON.stringify($cssTextarea.val()));
+    }
+    $extraStylesContainer.html($cssTextarea.val());
+}
+
+var clearCSSTextarea = function() {
+    $cssTextarea.val('');
+}
+
+var checkForTabbing = function() {
+    if(e.keyCode === 9) {
+        e.preventDefault();
+        $(this).val($(this).val() + '    ');
+    }
+}
+
+var updateWindowDimensions = function() {
+    windowWidth = $window.width();
+}
+
 $(function() {
-    $('#add_css').click(function() {
-        $('#css_dialog').fadeToggle();
-        $('#dialog_overlay').fadeToggle();
-    });
-    $('#btn_save').click(function() {
-        if($('#remember_css').is(':checked')) {
-            localStorage.setItem('css-styles', JSON.stringify($('#css_textarea').val()));
-        }
-        $('#extra_styles').html($('#css_textarea').val());
-    });
-    $('#btn_clear').click(function() {
-        $('#css_textarea').val('');
-    });
-    $('#dialog_overlay').click(function() {
-        $('#css_dialog').fadeToggle();
-        $('#dialog_overlay').fadeToggle();
-    });
-    $('#code_view').on('keyup keydown paste', setDelay).focus();
-    $('textarea').keydown(function(e) {
-        if(e.keyCode === 9) {
-            e.preventDefault();
-            $(this).val($(this).val() + '    ');
-        }
-    });
+
+    extendJquery();
+
+    $textarea = $('#textarea');
+    $addCSS = $('#add_css');
+    $btnSave = $('#btn_save');
+    $btnClear = $('#btn_clear');
+    $dialogOverlay = $('#dialog_overlay');
+    $dialogyCSS = $('#dialog_css');
+    $codeView = $('#code_view');
+    $rememberCSSCheckbox = $('#remember_css');
+    $preview = $('#preview').contents().find('#content');
+    $extraStylesContainer = $preview.next('#extra_styles_container');
+    $cssTextarea = $('#css_textarea');
+    $ruler = $('#ruler');
+    $window = $(window);
+
+    updateWindowDimensions();
+
+    $addCSS.on('click', showHideDialog);
+    $btnSave.on('click', saveStyles);
+    $btnClear.on('click', clearCSSTextarea);
+    $dialogOverlay.on('click', showHideDialog);
+    $codeView.on('keyup keydown paste', setDelay).focus();
+    $textarea.keydown(checkForTabbing);
+    $window.resize(updateWindowDimensions);
+
+    // TIDY THIS FUCKING SHIT UP!!!! ERRRRRRRRRRRRGH
+    $ruler
+        .on('mousedown', function() {
+            $window
+                .on('mousemove', function(e) {
+                    var fromLeft = e.clientX;
+                    if(fromLeft > 200 && fromLeft < windowWidth - 200) {
+                        $ruler.css({'left': perc(fromLeft, windowWidth)});
+                        $codeView.css({'width': perc(fromLeft, windowWidth)});
+                        $preview.css({'width': perc(windowWidth - fromLeft, windowWidth)});
+                    }
+                })
+                .on('mouseup', function() {
+                    rulerActive = false;
+                    $window
+                        .off('mousemove')
+                        .off('mouseup');
+                });
+
+        }).disableSelection();
+
+
+    // make a request if there is content in the box already
+    if($codeView.val() != '') setDelay();
 });
+
+function perc(a, b) {
+    console.log(a, b);
+    console.log(parseInt(a / b * 100, 10) + '%');
+    return parseInt(a / b * 100, 10) + '%';
+}
